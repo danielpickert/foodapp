151bd246b716bd350dfe36f8cc56bc8dda360eea
Michelle Monfort <Michellemnfrt@gmail.com>
2018-07-11 14:45:03 -0400

    added home icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import logo from "./logo.svg";
+import logo from "../logo.svg";
 import "../Style/Header.css";
 
 class Header extends Component {
@@ -7,11 +7,13 @@ class Header extends Component {
     return (
       <div className="Header">
         <header className="Header-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <img
+            src="https://comps.canstockphoto.com/recipes-3d-concept-stock-illustration_csp9417848.jpg"
+            className="App-logo"
+            alt="logo"
+          />
           <h1 className="Header-title">Welcome The Recipe Hub</h1>
-          <div> A project designed Us  </div>
-          
-         
+          <div> A project designed Us </div>
         </header>
         <p className="Header-intro">
           To get started, click on the recipe that you want to browse.
