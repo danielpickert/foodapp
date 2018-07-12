3cfb4efd7680f7e02eae74bdbf50bcff2c24e8ea
Daniel Pickert <dpickert1@gmail.com>
2018-07-11 23:57:43 -0400

    fixed errors, added functions, got server running again

--- src/App.js
+++ src/App.js
@@ -5,27 +5,63 @@ import "./Style/App.css";
 // import Recipe from "./components/Recipe.js";
 // import Header from "./components/Header.js";
 // import Search from "./components/Search";
-import Display from "./components/Display.js";
-import Recipe from "./components/Recipe.js";
+// import Display from "./components/Display.js";
+// import Recipe from "./components/Recipe.js";
 import axios from 'axios';
 
 // // API KEY for Food Recipes
 // const FOOD_KEY = 'e932a4dbf99f934fb4163d7391dc9865';
 
 
-
 class App extends Component {
-  state = {
-    title: undefined
-  }
+  constructor() {
+    super() 
+      this.state = {
+        name: undefined
+
+    }
+    
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  
+}
+
+handleChange = event => {
+  this.setState({ name: event.target.value });
+};
+
+handleSubmit = event => {
+  event.preventDefault();
+
+  const recipe = {
+    name: this.state.name,
+  };
 
 
+ axios.post("http://localhost:3000", {recipe})
+   // .then(reply => {
+   //   res.json(reply.data.recipes);
+   // })
+      .then(res=> {
+        console.log(res)
+      })
+
+};
+
+
+
+ 
 componentDidMount() {
   axios.get("http://localhost:3000/search/salt")
     .then(res => {
-    console.log(res)
-    })
-}
+    console.log(res.data)
+    this.setState({ img: res.data });
+      // this.setState({ title.value })
+    });
+  }
+
+
+
 
   render() {
     return (
@@ -43,9 +79,13 @@ componentDidMount() {
         <div className="Search">
           To get started, search for a recipe!
 
-          <form action="/search/" method="GET">
-            <input style={{ margin:"10px auto", display: "block", width: "140px" }} type="text" name="title" placeholder="Name of dish or ingredient" />
-            <input type="submit" value="Submit" class="btn" />
+          {this.state.name}
+
+          <form onSubmit={this.handleSubmit} action="/search/" method="GET">
+            <label>
+            <input style={{ margin:"10px auto", display: "block", width: "140px" }} type="text" name="name" onChange={this.handleChange} placeholder="Name of dish or ingredient" />
+            <button type="submit">Submit</button>
+            </label>
           </form>
         </div>
 
@@ -59,6 +99,10 @@ componentDidMount() {
     
     );
   }
+
+
 }
+
+
 
 export default App;
