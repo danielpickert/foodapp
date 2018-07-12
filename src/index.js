07fa7f250ecf27047e972464fa9fef30bf19511e
Daniel Pickert <dpickert1@gmail.com>
2018-07-12 18:19:58 -0400

    deleted unnecessary index.css file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import Router from "./components/Router"
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import registerServiceWorker from './registerServiceWorker';
