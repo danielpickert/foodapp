2bb106f68feefd286bd489a4090814db2389f278
Daniel Pickert <dpickert1@gmail.com>
2018-07-11 20:26:03 -0400

    changed webpage to just header and search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 // import logo from "./logo.svg";
 import "./Style/App.css";
-import Display from "./components/Display.js";
-import Recipe from "./components/Recipe.js";
-import Header from "./components/Header.js";
-import Search from "./components/Search";
+// import Display from "./components/Display.js";
+// import Recipe from "./components/Recipe.js";
+// import Header from "./components/Header.js";
+// import Search from "./components/Search";
 
 class App extends Component {
   render() {
@@ -13,25 +13,30 @@ class App extends Component {
         <header className="App-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h1 className="App-title">Welcome to The Recipe Hub</h1>
-          <div> A project designed by </div>
-          <h2>Daniel </h2>
-          <h2>Maseeh </h2>
-          <h2> && Michelle </h2>
+          <img
+            src="https://comps.canstockphoto.com/recipes-3d-concept-stock-illustration_csp9417848.jpg"
+            className="App-logo"
+            alt="logo"
+          />
         </header>
         
-        <p className="App-intro">
+        <div className="Search">
           To get started, search for a recipe!
 
           <form action="/search/" method="GET">
             <input style={{ margin:"10px auto", display: "block", width: "140px" }} type="text" name="title" placeholder="Name of dish or ingredient" />
             <input type="submit" value="Submit" class="btn" />
           </form>
-          <Display name="The dish you selected" />
-          <Recipe name="Burger" />
-          <Header name="Header" />
-          <Search name="Search" />
-        </p>
-      </div>
+        </div>
+
+          </div>  
+          // className="App-intro">
+          // // <Display name="The dish you selected" />
+          // <Recipe name="Burger" />
+          // <Header name="Header" />
+          // <Search name="Search" />
+          // </p>
+    
     );
   }
 }
